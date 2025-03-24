a080a92a52cc620e6c7b8c74d82ec028e1b8fde7
Andrea <Iskrea.Andrea@gmail.com>
2025-03-24 16:55:48 +0100

    TQR matrix done

--- triple-qr-code-gen/tqr_gen.py
+++ triple-qr-code-gen/tqr_gen.py
@@ -1,7 +1,10 @@
 from qr_gen import QR, get_capacity_value, make_2d_array
 
-
 def make_tqr(text0, text1, text2):
+    def combine_qrs():
+        matrix0, matrix1, matrix2 = qr_matrix0, qr_matrix1 * 2, qr_matrix2 * 4
+        return matrix0 + matrix1 + matrix2
+
     qr0, qr1, qr2 = QR(text0), QR(text1), QR(text2)
     largest_val = max(qr0.data_len, qr1.data_len, qr2.data_len)
     level = get_capacity_value(largest_val)
@@ -9,3 +12,4 @@ def make_tqr(text0, text1, text2):
     qr1.set_level_and_fill(level)
     qr2.set_level_and_fill(level)
     qr_matrix0, qr_matrix1, qr_matrix2 = make_2d_array(qr0), make_2d_array(qr1), make_2d_array(qr2)
+    return combine_qrs()
