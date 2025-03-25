02cf8e26388bf1c2c9fec0f5a146a4ed6a6c9f24
Andrea <Iskrea.Andrea@gmail.com>
2025-03-25 17:23:54 +0100

    TQR image drawn

--- triple-qr-code-gen/utils.py
+++ triple-qr-code-gen/utils.py
@@ -1,17 +1,31 @@
 from PIL import Image
-from enum import Enum
 
-class Color(Enum):
-    NONE = 0
-    FIRST = 1
-    SECOND = 2
-    THIRD = 4
-    FCOMBOS = 3
-    FCOMBOT = 5
-    SCOMBOT = 6
-    COMBO = 7
+from tqr_gen import make_tqr
 
-def draw(qr, color1, color2, color3):
-    qr_image = Image.new('RGBA', (250, 250), "transparent")
 
-def create_qr_image(text0, text1):
+def get_color(num):
+    nums_and_colors = {
+        0: (255, 255, 255),
+        1: (255, 255, 0),
+        2: (255, 0, 0),
+        3: (255, 100, 0),
+        4: (0, 0, 255),
+        5: (0, 255, 0),
+        6: (255, 0, 255),
+        7: (0, 0, 0)
+    }
+    return nums_and_colors[num]
+
+
+def create_qr_image(text0, text1, text2):
+    def draw_image():
+        size = len(tqr_matrix)
+        tqr_image = Image.new('RGB', (size, size))
+        for x in range(size):
+            for y in range(size):
+                color = get_color(tqr_matrix[x][y])
+                tqr_image.putpixel((x, y), color)
+        return tqr_image
+
+    tqr_matrix = make_tqr(text0, text1, text2)
+    return draw_image()
