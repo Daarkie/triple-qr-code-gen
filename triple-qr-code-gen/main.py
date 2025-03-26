1d6f0c78cead03d3510fc1c1e52eff63105e2311
Andrea <Iskrea.Andrea@gmail.com>
2025-03-26 18:41:22 +0100

    TQR image is being shown with colors

--- triple-qr-code-gen/main.py
+++ triple-qr-code-gen/main.py
@@ -4,7 +4,6 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from win32con import TRUETYPE_FONTTYPE
 
 from utils import create_tqr_image
 
