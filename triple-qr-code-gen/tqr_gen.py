9ae550168292180de1c16e2ffd9d8576b7f42887
Andrea <Iskrea.Andrea@gmail.com>
2025-04-01 19:40:09 +0200

    Amended imports

--- triple-qr-code-gen/tqr_gen.py
+++ triple-qr-code-gen/tqr_gen.py
@@ -1,5 +1,4 @@
 import numpy as np
-import qrcodegen
 from qrcodegen import *
 
 
@@ -10,7 +9,7 @@ def make_tqr(text0, text1, text2):
 
     text_list = [text0, text1, text2]
     list.sort(text_list, key=len)
-    qr_matrix0 = qrcodegen.QrCode.encode_text(text_list[0], QrCode.Ecc.LOW)
+    qr_matrix0 = QrCode.encode_text(text_list[0], QrCode.Ecc.LOW)
     qr_matrix1, qr_matrix2 = None, None
     version0 = qr_matrix0.get_version()
     error_correction = [QrCode.Ecc.LOW, QrCode.Ecc.MEDIUM, QrCode.Ecc.QUARTILE, QrCode.Ecc.HIGH]
