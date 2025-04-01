dd892039f2cfa22bb8b510da8c965951c0b20b6e
Andrea <Iskrea.Andrea@gmail.com>
2025-04-01 19:39:03 +0200

    Check for best versions

--- triple-qr-code-gen/tqr_gen.py
+++ triple-qr-code-gen/tqr_gen.py
@@ -11,8 +11,20 @@ def make_tqr(text0, text1, text2):
     text_list = [text0, text1, text2]
     list.sort(text_list, key=len)
     qr_matrix0 = qrcodegen.QrCode.encode_text(text_list[0], QrCode.Ecc.LOW)
-    qr_matrix1 = qrcodegen.QrCode.encode_segments(QrSegment.make_segments(text_list[1]), QrCode.Ecc.LOW,
-                                                  minversion=qr_matrix0.get_version())
-    qr_matrix2 = qrcodegen.QrCode.encode_segments(QrSegment.make_segments(text_list[2]), QrCode.Ecc.LOW,
-                                                  minversion=qr_matrix0.get_version())
+    qr_matrix1, qr_matrix2 = None, None
+    version0 = qr_matrix0.get_version()
+    error_correction = [QrCode.Ecc.LOW, QrCode.Ecc.MEDIUM, QrCode.Ecc.QUARTILE, QrCode.Ecc.HIGH]
+    version2 = float("inf")
+    level = 4
+    while version2 > version0:
+        level -= 1
+        qr_matrix2 = QrCode.encode_segments(QrSegment.make_segments(text_list[2]), error_correction[level],
+                                                      minversion=version0)
+        version2 = qr_matrix2.get_version()
+    version1 = version2 + 1
+    while version1 > version0:
+        level -= 1
+        qr_matrix1 = QrCode.encode_segments(QrSegment.make_segments(text_list[1]), error_correction[level],
+                                                      minversion=version0)
+        version1 = qr_matrix1.get_version()
     return combine_qrs(qr_matrix0, qr_matrix1, qr_matrix2)
